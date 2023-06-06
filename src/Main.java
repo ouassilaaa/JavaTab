9ba06d510230cc070398be0fb9821f5cb09136e6
ouassilaaa <ouassidu3@hotmail.fr>
2023-06-06 09:59:46 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //les tableaux ne peuvent contenir qu'un seul type, pas de mélange
+        int [] tab ={1,2,3,4};
+        int nb1 =1;
+        //c'est l'emplacement mémoire qui sera printé et non le tableau, il faudra utiliser le deboguer pour accéder au tableau
+
+        System.out.println(tab);
+        }
+    }
